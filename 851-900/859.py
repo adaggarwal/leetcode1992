629ad926fb10017fb1120f9881e85bf786b2ba27
adityaaggarwal1992 <adimsuf@gmail.com>
2018-06-24 15:30:08 -0700

    Added comments.

--- 851-900/859.py
+++ 851-900/859.py
@@ -1,36 +1,37 @@
-#Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
-#
-# 
-#
-#Example 1:
-#
-#Input: A = "ab", B = "ba"
-#Output: true
-#Example 2:
-#
-#Input: A = "ab", B = "ab"
-#Output: false
-#Example 3:
-#
-#Input: A = "aa", B = "aa"
-#Output: true
-#Example 4:
-#
-#Input: A = "aaaaaaabc", B = "aaaaaaacb"
-#Output: true
-#Example 5:
-#
-#Input: A = "", B = "aa"
-#Output: false
-# 
-#
-#Note:
-#
-#0 <= A.length <= 20000
-#0 <= B.length <= 20000
-#A and B consist only of lowercase letters.
-
 '''
+Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+
+ 
+
+Example 1:
+
+Input: A = "ab", B = "ba"
+Output: true
+Example 2:
+
+Input: A = "ab", B = "ab"
+Output: false
+Example 3:
+
+Input: A = "aa", B = "aa"
+Output: true
+Example 4:
+
+Input: A = "aaaaaaabc", B = "aaaaaaacb"
+Output: true
+Example 5:
+
+Input: A = "", B = "aa"
+Output: false
+ 
+
+Note:
+
+0 <= A.length <= 20000
+0 <= B.length <= 20000
+A and B consist only of lowercase letters.
+
+
 Lets start with eliminating false corner cases like
 1. Length of A not equals to B
 2. if 1. is true then length of both A and B is equal; now lets check when length of both of them is 1 and if A == B or not
