78e328a40b8258a88b9f36de138250423e1e9d7f
adityaaggarwal1992 <adimsuf@gmail.com>
2018-06-29 20:59:57 -0700

    Added 20. Valid Parentheses

---
+++ 1-50/20.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = list(s)
+        par = {
+                '(':')',
+                '{':'}',
+                '[':']'
+        }
+        result = []
+        for elem in s:
+            if elem in par.values():
+                if len(result) and elem == par[result[-1]]:
+                    result.pop()
+                else:
+                    return False
+            else:
+                result.append(elem)
+        return True if len(result) == 0 else False
