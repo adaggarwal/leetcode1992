1747236c43353aeb312868b008d1329d8177290d
adityaaggarwal1992 <aggaraditya@gmail.com>
2018-10-18 01:27:59 -0700

    Added 703.py

---
+++ 701-750/703.py
@@ -0,0 +1,30 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) - k > 0:
+            heapq.heappop(self.heap)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if len(self.heap) == self.k:
+            heapq.heappushpop(self.heap, val)
+        else:
+            heapq.heappush(self.heap, val)
+        return self.heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
