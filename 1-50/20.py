8fb6d794d7425eaab48b0fa71816a3a0426fff44
adityaaggarwal1992 <aggaraditya@gmail.com>
2018-10-21 20:16:06 -0700

    Add 20.py

--- 1-50/20.py
+++ 1-50/20.py
@@ -31,26 +31,26 @@ Input: "{[]}"
 Output: true
 
 '''
-
 class Solution:
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        s = list(s)
-        par = {
-                '(':')',
-                '{':'}',
-                '[':']'
+        hashmap = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
         }
-        result = []
-        for elem in s:
-            if elem in par.values():
-                if len(result) and elem == par[result[-1]]:
-                    result.pop()
+        stack = []
+        
+        for el in s:
+            if el not in hashmap:
+                stack.append(el)
+            else:
+                if stack:
+                    if hashmap[el] != stack.pop():
+                        return False
                 else:
                     return False
-            else:
-                result.append(elem)
-        return True if len(result) == 0 else False
+        return False if stack else True
