f42e792790f2cf7a542df4a7b6e841b1c9015e8f
adityaaggarwal1992 <aggaraditya@gmail.com>
2018-11-01 18:39:14 -0700

    Edit 305

--- 301-350/305.py
+++ 301-350/305.py
@@ -1,4 +1,6 @@
 '''
+305. Number of Islands II
+
 A 2d grid map of m rows and n columns is initially filled with water. We may perform an addLand operation which turns the water at position (row, col) into a land. Given a list of positions to operate, count the number of islands after each addLand operation. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 Example:
