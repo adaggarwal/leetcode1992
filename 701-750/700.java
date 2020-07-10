c381f22f2ae45c942051e4865735391e187365b0
akanshamalik <akansha.96malik@gmail.com>
2020-07-09 20:47:06 -0500

    add: solution for 700

---
+++ 701-750/700.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+         while(root != null && root.val != val){
+            root = val<root.val? root.left:root.right;
+        }
+        return root;
+    }
+}
